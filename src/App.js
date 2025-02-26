b9648747c9568d8f0e0fd6e8a2a090a7d8085370
Pacheko <patrickoluochex@gmail.com>
2025-02-26 16:24:56 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Video from './components/Video';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-2 mt-4'>
+      <Navbar/>
+      <Hero/>
+      <Video/>
     </div>
+    
+    
   );
 }
 
